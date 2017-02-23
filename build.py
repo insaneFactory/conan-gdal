9df53b413975775689e1e04f767d07eb176c7700
Olivier Sechet <osechet@gmail.com>
2017-02-23 18:40:40 +0100

    Add debug build to travis builds

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     builder.add_common_builds()
     filtered_builds = []
     for settings, options in builder.builds:
-        if settings["arch"] == "x86_64" and settings["build_type"] != "Debug":
+        if settings["arch"] == "x86_64":
             filtered_builds.append([settings, options])
     builder.builds = filtered_builds
     builder.run()

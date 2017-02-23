b63e1f1e9257979b335d481119c35c5d0c30640d
osechet <osechet@gmail.com>
2017-02-23 11:16:30 +0100

    Add build.py file

---
+++ build.py
@@ -0,0 +1,14 @@
+
+import platform
+from conan.packager import ConanMultiPackager
+from conans.tools import os_info
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager(username="osechet", channel="stable")
+    builder.add_common_builds()
+    filtered_builds = []
+    for settings, options in builder.builds:
+        if settings["arch"] == "x86_64":
+            filtered_builds.append([settings, options])
+    builder.builds = filtered_builds
+    builder.run()

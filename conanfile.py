437f24b68b54c6ece0e532014b3a607f107a9c77
mstein <m.stein@h3-ms.com>
2019-10-15 18:05:35 +0200

    Adds dependencies through conan. (WIP)

--- conanfile.py
+++ conanfile.py
@@ -18,19 +18,24 @@ class GdalConan(ConanFile):
     }
     requires = (
         "zlib/1.2.11@conan/stable",
-        "sqlite3/3.29.0@bincrafters/stable"
+        "sqlite3/3.29.0@bincrafters/stable",
+		"libjpeg/9c@bincrafters/stable",
+        "openjpeg/2.3.1@bincrafters/stable",
+		"libpng/1.6.34@bincrafters/stable",
+		"libxml2/2.9.8@bincrafters/stable",
+		"libtiff/4.0.9@bincrafters/stable",
+		"libgeotiff/1.4.2@insanefactory/stable",
+        "giflib/5.1.4@bincrafters/stable",
+		"proj4/5.2.0@insanefactory/stable",
+        "geos/3.8.0@insanefactory/stable"
     )
     exports = ["LICENSE.md", "FindGDAL.cmake"]
+    #generators = "pkg_config"
     _source_subfolder = "source_subfolder"
 
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
-
-    def source(self):
-		name = "osgearth"
-        tools.get("{0}/{1}-{2}.tar.gz".format(self.download_prefix, name, self.version))
-        extracted_dir = "{}-{}-".format(name, name) + self.version
 
     def source(self):
         archive_name = "gdal-%s" % self.version
@@ -41,22 +46,50 @@ class GdalConan(ConanFile):
             self.run("chmod +x ./%s/configure" % self._source_subfolder)
 
     def _build_autotools(self):
-        config_args = ["--with-geos=yes"]
+        # Args
+        config_args = [
+            "--with-libz",
+            "--with-sqlite3",
+            "--with-jpeg",
+            "--with-openjpeg",
+            "--with-png",
+            "--with-xml2",
+            "--with-libtiff",
+            "--with-geotiff",
+            "--with-gif",
+            "--with-proj",
+            "--with-proj5-api=no",
+            "--with-geos"
+        ]
+
         if self.options.shared:
-            config_args += ["--disable-static", "--enable-shared"]
+            config_args.append("--disable-static")
+            config_args.append("--enable-shared")
         else:
-            config_args += [
-                "--without-ld-shared", "--disable-shared", "--enable-static",
-            ]
+            #config_args.append("--without-ld-shared")
+            config_args.append("--disable-shared")
+            config_args.append("--enable-static")
+
+        # LD_LIBRARY_PATH
+        ld_library_path = ":".join(self.deps_cpp_info.lib_paths)
+        env_ld_library_path = tools.get_env("LD_LIBRARY_PATH")
+        if env_ld_library_path is not None and len(env_ld_library_path) > 0:
+            ld_library_path += ":" + env_ld_library_path
 
         autotools = AutoToolsBuildEnvironment(self)
+        autotools_vars = autotools.vars
+        if "LD_LIBRARY_PATH" in autotools_vars and len(autotools_vars["LD_LIBRARY_PATH"]) > 0:
+            ld_library_path += ":" + autotools_vars["LD_LIBRARY_PATH"]
+        autotools_vars["LD_LIBRARY_PATH"] = ld_library_path
+
+        # Build
         with tools.chdir(self._source_subfolder):
-            autotools.configure(args=config_args)
+            autotools.configure(args=config_args, vars=autotools_vars)
             autotools.make()
             autotools.install()
 
         self.run("cp %s/FindGDAL.cmake %s/" % (self.source_folder, self.package_folder))
-        
+
     def _build_vs(self):
         versions = {
             "14": "1900",
@@ -64,7 +97,7 @@ class GdalConan(ConanFile):
             "16": "1920"
         }
         vsver = versions.get(self.settings.compiler.version, None)
-        
+
         args = "GDAL_HOME=" + self.package_folder
         if vsver is not None:
             args += " MSVC_VER=%s" % vsver
@@ -74,13 +107,13 @@ class GdalConan(ConanFile):
             args += " WIN64=1"
         if not self.options.shared:
             args += " DLLBUILD=0"
-        
+
         env_build = VisualStudioBuildEnvironment(self)
         with tools.environment_append(env_build.vars):
             vcvars = tools.vcvars_command(self.settings)
             self.run("%s && nmake -f makefile.vc %s" % (vcvars, args))
             self.run("%s && nmake -f makefile.vc %s install" % (vcvars, args))
-        
+
     def build(self):
         if self.settings.compiler == "Visual Studio":
             self._build_vs()
